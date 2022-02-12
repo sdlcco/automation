0a9077449ee087ba40428d387ce74ebdd45b0fd3
Brutalation <brutalation@gmail.com>
2022-02-12 10:00:16 -0500

    Update StepDefinitions.java

--- src/main/java/glue/StepDefinitions.java
+++ src/main/java/glue/StepDefinitions.java
@@ -287,17 +287,6 @@ public void method11(String param241, String param242) throws Throwable {
 myDriver.verifyObjectPresentOnPage(param241, param242);//<CODE>
 }
 
- @Then("^user navigates to \"([^\"]*)\" in new tab$")
-public void method11(String param141) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//navigation3
-jse.executeScript("window.open('"+param141+"', '_blank');");//navigation3
-jse.executeScript("window.open('"+'"+param141+"'+"', '_blank');");//<CODE>
-}
-
- @And("^user refreshes tab$")
-public void method12() throws Throwable {
-// Write code here that turns the phrase above into concrete actions//<CODE>
-}
 
  //<METHOD>
 
