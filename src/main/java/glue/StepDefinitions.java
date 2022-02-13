f09a3217ca6a63c7dd3eaab74bbbc083b8155902
Brutalation <brutalation@gmail.com>
2022-02-13 12:06:41 -0500

    Commtting File: StepDefinitionFile

--- src/main/java/glue/StepDefinitions.java
+++ src/main/java/glue/StepDefinitions.java
@@ -258,27 +258,43 @@ myDriver.clickOnPage(param21, param22);//<CODE>
   chooseFile.sendKeys(location);
  }
 
- @Then("^user navigates to \"([^\"]*)\" in new tab$")
-public void method6(String param131) throws Throwable {
+ @And("^user clicks \"([^\"]*)\" button on \"([^\"]*)\" page$")
+public void method7(String param21, String param22) throws Throwable {
+// Write code here that turns the phrase above into concrete actions//button1
+myDriver.clickOnPage(param21, param22);//<CODE>
+}
+
+ @Then("^user navigates to \"([^\"]*)\"$")
+public void method8(String param71) throws Throwable {
 // Write code here that turns the phrase above into concrete actions//navigation2
-driver.get(param131);//<CODE>
+driver.get(param71);//<CODE>
+}
+
+ @Then("^user navigates to \"([^\"]*)\" in new tab$")
+public void method9(String param131) throws Throwable {
+// Write code here that turns the phrase above into concrete actions//navigation3
+String url = param131;
+jse.executeScript("window.open('"+url+"', '_blank');");//<CODE>
 }
 
  @Then("^user switches to latest tab$")
-public void method7() throws Throwable {
+public void method10() throws Throwable {
 // Write code here that turns the phrase above into concrete actions
 //<CODE>
 }
 
  @And("^user refreshes tab$")
-public void method8() throws Throwable {
+public void method11() throws Throwable {
 // Write code here that turns the phrase above into concrete actions
 //<CODE>
 }
 
  @Then("^user waits for \"([^\"]*)\" seconds$")
-public void method9(String param171) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//<CODE>
+public void method12(String param171) throws Throwable {
+// Write code here that turns the phrase above into concrete actions//waits1
+int seconds = Integer.parseInt(param171);
+int milliseconds = 1000;
+Thread.sleep(seconds*milliseconds);//<CODE>
 }
 
  //<METHOD>
