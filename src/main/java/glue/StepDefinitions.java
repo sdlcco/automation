fa407f85563325526481235d1898009a982a49c1
Brutalation <brutalation@gmail.com>
2022-02-13 08:10:04 -0500

    Update StepDefinitions.java

--- src/main/java/glue/StepDefinitions.java
+++ src/main/java/glue/StepDefinitions.java
@@ -259,35 +259,6 @@ myDriver.clickOnPage(param21, param22);//<CODE>
  }
 
 
- @Then("^user navigates to \"([^\"]*)\" in new tab$")
-public void method7(String param131) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//waits1
-int seconds = Integer.parseInt(param131);
-int milliseconds = 1000;
-Thread.sleep(seconds*milliseconds);//<CODE>
-}
-
- @Then("^user switches to latest tab$")
-public void method8() throws Throwable {
-// Write code here that turns the phrase above into concrete actions//navigation5
-int count = driver.getWindowHandles().size();
-driver.switchTo().window(driver.getWindowHandles().stream().skip(count - 1).findFirst().get());//<CODE>
-}
-
- @And("^user refreshes tab$")
-public void method9() throws Throwable {
-// Write code here that turns the phrase above into concrete actions//navigation4
-driver.navigate().refresh();//<CODE>
-}
-
- @Then("^user waits for \"([^\"]*)\" seconds$")
-public void method10(String param171) throws Throwable {
-// Write code here that turns the phrase above into concrete actions//waits1
-int seconds = Integer.parseInt(param171);
-int milliseconds = 1000;
-Thread.sleep(seconds*milliseconds);//<CODE>
-}
-
  //<METHOD>
 
 
