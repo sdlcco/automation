5e035b6a9c41fa470441c0ab58b263974bb14c57
Brutalation <brutalation@gmail.com>
2022-02-13 07:22:50 -0500

    Commtting File: StepDefinitionFile

--- src/main/java/glue/StepDefinitions.java
+++ src/main/java/glue/StepDefinitions.java
@@ -288,6 +288,28 @@ myDriver.verifyObjectPresentOnPage(param241, param242);//<CODE>
 }
 
 
+ @Then("^user navigates to \"([^\"]*)\" in new tab$")
+public void method11(String param141) throws Throwable {
+// Write code here that turns the phrase above into concrete actions//navigation3
+String url = param141;
+jse.executeScript("window.open('"+url+"', '_blank');");//<CODE>
+}
+
+ @And("^user navigates to latest tab$")
+public void method12() throws Throwable {
+// Write code here that turns the phrase above into concrete actions//navigation5
+int count = driver.getWindowHandles().size();
+driver.switchTo().window(driver.getWindowHandles().stream().skip(count - 1).findFirst().get());//<CODE>
+}
+
+ @And("^user refreshes tab$")
+public void method13() throws Throwable {
+// Write code here that turns the phrase above into concrete actions////navigation5
+
+int count = driver.getWindowHandles().size();
+driver.switchTo().window(driver.getWindowHandles().stream().skip(count - 1).findFirst().get());//<CODE>
+}
+
  //<METHOD>
 
 
