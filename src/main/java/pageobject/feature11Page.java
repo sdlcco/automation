c90aab142a91857f717a2364810752a36bc76926
Brutalation <brutalation@gmail.com>
2022-02-13 12:06:41 -0500

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature11Page.java
+++ src/main/java/pageobject/feature11Page.java
@@ -21,21 +21,23 @@ public static Properties properties;
 /*Xpaths*/
 public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
 public static By searchBox = By.xpath("//input[@title='Search']");
-public static By SwitchChainButton = By.xpath("//i[@class='network-display__icon app-header__network-down-arrow']");
+public static By button50 = By.xpath("//i[@class='network-display__icon app-header__network-down-arrow']");
 
-public static By AvaxButton = By.xpath("(//span[.='Avalanche Mainnet C-Chain'])[last()]");
+public static By button70 = By.xpath("(//span[.='Avalanche Mainnet C-Chain'])[last()]");
 
-public static By ClaimRewardsButton = By.xpath("//span[.='Claim Rewards']/..");
+public static By button90 = By.xpath("//span[.='Claim Rewards']/..");
 
-public static By ConfirmButton = By.xpath("//button[.='Confirm']");
+public static By button120 = By.xpath("//button[.='Confirm']");
 
-public static By BinanceSmartChainButton = By.xpath("(//span[.='Binance Smart Chain'])[last()]");
+public static By button140 = By.xpath("(//span[.='Binance Smart Chain'])[last()]");
 
-public static By ConnectWalletButton = By.xpath("//button[.='Connect Wallet']");
+public static By button160 = By.xpath("//button[.='Connect Wallet']");
 
-public static By ConnectToMetamaskButton = By.xpath("//div[.='Connect to your MetaMask Wallet']");
+public static By button170 = By.xpath("//div[.='Connect to your MetaMask Wallet']");
 
-public static By HydrateButton = By.xpath("//button[contains(.,'HYDRATE')]");
+public static By button190 = By.xpath("//button[contains(.,'HYDRATE')]");
+
+public static By button230 = By.xpath("//button[.='Confirm']");
 
 //<XPATHS>
 
