84d576327ffcec501b6bb48d08fc3e686e0a49d6
Brutalation <brutalation@gmail.com>
2022-02-13 08:47:14 -0500

    Commtting File: StepDefinitionFile

--- src/main/java/glue/StepDefinitions.java
+++ src/main/java/glue/StepDefinitions.java
@@ -261,6 +261,46 @@ myDriver.clickOnPage(param21, param22);//<CODE>
  }
 
 
+ @Given("^user navigates to \"([^\"]*)\" in new tab$")
+public void method6(String param11) throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//navigation3
+String url = param11;
+jse.executeScript("window.open('"+url+"', '_blank');");//<CODE>
+}
+
+ @And("^user switches to new tab$")
+public void method7() throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//navigation5
+int count = driver.getWindowHandles().size();
+driver.switchTo().window(driver.getWindowHandles().stream().skip(count - 1).findFirst().get());//<CODE>
+}
+
+ @And("^user refreshes tab$")
+public void method8() throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//navigation4
+driver.navigate().refresh();//<CODE>
+}
+
+ @Then("^user switches to latest tab$")
+public void method9() throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//navigation5
+int count = driver.getWindowHandles().size();
+driver.switchTo().window(driver.getWindowHandles().stream().skip(count - 1).findFirst().get());//<CODE>
+}
+
+ @Then("^user waits for \"([^\"]*)\" seconds$")
+public void method10(String param191) throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//waits1
+int seconds = Integer.parseInt(param191);
+int milliseconds = 1000;
+Thread.sleep(seconds*milliseconds);//<CODE>
+}
+
  //<METHOD>
 
 
