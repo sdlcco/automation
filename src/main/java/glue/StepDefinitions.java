e486282bea81c0beeebf6f4934fa91b4d984ebf1
Brutalation <brutalation@gmail.com>
2022-03-03 16:06:34 -0500

    Update StepDefinitions.java

--- src/main/java/glue/StepDefinitions.java
+++ src/main/java/glue/StepDefinitions.java
@@ -296,37 +296,7 @@ public void method12(String param21) throws Throwable {
   int milliseconds = 1000;
   Thread.sleep(seconds * milliseconds);//<CODE>
 }
-
- @@Thor("^@Thor$")
-public void method13() throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-//<CODE>
-}
-
- @@Vapor("^@Vapor$")
-public void method14() throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-//<CODE>
-}
-
- @@Drip("^@Drip$")
-public void method15() throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-//<CODE>
-}
-
- @@Cubo("^@Cubo$")
-public void method16() throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-//<CODE>
-}
-
- @@Farm("^@Farm$")
-public void method17() throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-//<CODE>
-}
-
+ 
  //<METHOD>
 
 
