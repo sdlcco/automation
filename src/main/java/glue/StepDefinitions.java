fa8c59944b9e5a9fb0efa1d12d695a5cd2251091
Brutalation <brutalation@gmail.com>
2022-03-03 16:22:13 -0500

    Commtting File: StepDefinitionFile

--- src/main/java/glue/StepDefinitions.java
+++ src/main/java/glue/StepDefinitions.java
@@ -253,7 +253,10 @@ myDriver.clickOnPage(param21, param22);//<CODE>
  @And("^user waits \"([^\"]*)\" seconds$")
 public void method6(String param61) throws Throwable {
 // Write code here that turns the phrase above into concrete actions//waits1
-int seconds = Integer.parseInt(param61);\nint milliseconds = 1000;\nThread.sleep(seconds*milliseconds);//<CODE>
+int seconds = Integer.parseInt(param61);\nint milliseconds = 1000;\nThread.sleep(seconds*milliseconds);//waits1
+ int seconds = Integer.parseInt(param21);
+  int milliseconds = 1000;
+  Thread.sleep(seconds * milliseconds);//<CODE>
 }
 
  @Then("^user switches to latest tab$")
@@ -289,6 +292,36 @@ int count = driver.getWindowHandles().size();
 driver.switchTo().window(driver.getWindowHandles().stream().findFirst().get());//<CODE>
 }
 
+
+ @@Thor("^@Thor$")
+public void method12() throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//<CODE>
+}
+
+ @@Vapor("^@Vapor$")
+public void method13() throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//<CODE>
+}
+
+ @@Drip("^@Drip$")
+public void method14() throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//<CODE>
+}
+
+ @@Cubo("^@Cubo$")
+public void method15() throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//<CODE>
+}
+
+ @@Farm("^@Farm$")
+public void method16() throws Throwable {
+// Write code here that turns the phrase above into concrete actions
+//<CODE>
+}
 
  //<METHOD>
 
