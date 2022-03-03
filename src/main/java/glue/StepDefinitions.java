1cfce5952432546f9cd5c746ff86d1d86a31ad99
Brutalation <brutalation@gmail.com>
2022-03-03 16:23:42 -0500

    Update StepDefinitions.java

--- src/main/java/glue/StepDefinitions.java
+++ src/main/java/glue/StepDefinitions.java
@@ -292,37 +292,6 @@ int count = driver.getWindowHandles().size();
 driver.switchTo().window(driver.getWindowHandles().stream().findFirst().get());//<CODE>
 }
 
-
- @@Thor("^@Thor$")
-public void method12() throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-//<CODE>
-}
-
- @@Vapor("^@Vapor$")
-public void method13() throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-//<CODE>
-}
-
- @@Drip("^@Drip$")
-public void method14() throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-//<CODE>
-}
-
- @@Cubo("^@Cubo$")
-public void method15() throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-//<CODE>
-}
-
- @@Farm("^@Farm$")
-public void method16() throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-//<CODE>
-}
-
  //<METHOD>
 
 
