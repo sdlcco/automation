d12e21ea8b150eebc048caaea7f8140f8f7fe90d
Brutalation <brutalation@gmail.com>
2022-03-04 07:06:35 -0500

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature10Page.java
+++ src/main/java/pageobject/feature10Page.java
@@ -21,11 +21,9 @@ public static Properties properties;
 /*Xpaths*/
 public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
 public static By searchBox = By.xpath("//input[@title='Search']");
-public static By SwitchChainButton = By.xpath("//i[@class='network-display__icon app-header__network-down-arrow']");
+public static By MetamaskSwitchChainButton = By.xpath("//i[@class='network-display__icon app-header__network-down-arrow']");
 
 public static By AvaxNetworkButton = By.xpath("(//span[contains(.,'Avalanche Network')])[last()]");
-
-public static By ThorClaimRewardsButton = By.xpath("//span[.='Claim Rewards']/..");
 
 public static By ConfirmButton = By.xpath("//button[.='Confirm']");
 
@@ -33,15 +31,15 @@ public static By CompoundallButton = By.xpath("//button[contains(.,'Compound all
 
 public static By CompoundAllButton = By.xpath("//button[contains(.,'Compound All')][last()]");
 
-public static By BinanceSmartChainButton = By.xpath("(//span[contains(.,'Smart Chain')])[last()]");
+public static By BinanceNetworkButton = By.xpath("(//span[contains(.,'Smart Chain')])[last()]");
 
 public static By DripHydrateButton = By.xpath("//button[contains(.,'HYDRATE')]");
 
-public static By PolygonMainnetButton = By.xpath("(//span[.='Polygon Mainnet'])[last()]");
+public static By PolygonNetworkButton = By.xpath("(//span[.='Polygon Mainnet'])[last()]");
 
 public static By CuboCollectButton = By.xpath("//button[contains(.,'Collect')]");
 
-public static By HarvestButton = By.xpath("(//h2[text()='DOGS/WBNB'])[last()]/../../..//button[.='Harvest']");
+public static By FarmHarvestButton = By.xpath("(//h2[text()='DOGS/WBNB'])[last()]/../../..//button[.='Harvest']");
 
 //<XPATHS>
 
